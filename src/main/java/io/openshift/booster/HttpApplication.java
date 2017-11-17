7f13e586ea0369d618138d5c3a9c2dd87fa4f2d0
Clement Escoffier <clement.escoffier@gmail.com>
2017-11-17 14:28:47 +0100

    Update visibility of the template field

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hello, %s!";
+  static final String template = "Hello, %s!";
 
   @Override
   public void start(Future<Void> future) {
