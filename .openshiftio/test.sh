0323142bcc00a898d04394b4ec26ac4f69e5f074
Clement Escoffier <clement.escoffier@gmail.com>
2018-08-25 11:08:51 +0200

    add the management scripts

---
+++ .openshiftio/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+source openshift.sh
+
+if [ ! -d ".openshiftio" ]; then
+  warning "The script expects the .openshiftio directory to exist"
+  exit 1
+fi
+
+# Deploy the templates and required resources
+oc apply -f .openshiftio/application.yaml
+
+# Create the application
+oc new-app --template=vertx-http-booster -p SOURCE_REPOSITORY_URL=https://github.com/openshiftio-vertx-boosters/vertx-http-booster-redhat
+
+# wait for pod to be ready
+waitForPodState "http-vertx" "Running"
+waitForPodReadiness "http-vertx" 1
+
+mvn verify -Popenshift-it -Denv.init.enabled=false
